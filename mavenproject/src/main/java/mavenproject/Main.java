243671b0191d0ec953a374e7c0833e191c5fa7dc
jonathan.non <jonathan.non@CBEN-2HMNY93-LX.dir.svc.accenture.com>
2022-11-29 15:47:58 +0800

    Second commit

--- mavenproject/src/main/java/mavenproject/Main.java
+++ mavenproject/src/main/java/mavenproject/Main.java
@@ -1,9 +1,8 @@
 package mavenproject;
 
 public class Main {
-	
 	public static void main(String[] args) {
-		 int a = 100;
+		 int a = 1000;
 		 int b = 200;
 		System.out.printf("%d + %d = %d", a, b, a+b);
 	}
